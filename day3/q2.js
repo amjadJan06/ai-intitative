109bba1b6b146deee8a574faca8286437e40d22f
AMJAD JAN BAIG <164417812+amjadJan06@users.noreply.github.com>
2024-03-25 15:35:17 +0500

    add files via upload

---
+++ day3/q2.js
@@ -0,0 +1,4 @@
+console.log(4 + 33);
+console.log(30 - 22);
+console.log(22 * 4);
+console.log(10 / 2);
---
+++ day3/q3.js
@@ -0,0 +1,2 @@
+var favNum = "16";
+console.log("My favorite number is ".concat(favNum, "."));
